10b8f92c32f078ae2c6aed06c0c8a397b392d271
Mehmet <arkin.mehmet14@hotmail.com>
2021-06-16 10:58:05 +0200

    change

--- scheduler.py
+++ scheduler.py
@@ -1,7 +1,8 @@
+import numpy as np
+
 
 class Scheduler:
-
-    def __init___(grid, timestep=0, iteration_callback):
+    def __init___(self, grid, timestep=0, iteration_callback=None):
         """" Manages the timesteps on the circular grid
         Args:
             grid: the Circular grid object
@@ -15,30 +16,37 @@ class Scheduler:
         self.started = False
 
         self.history = []
-        selt.timestamp = 0
-
+        self.timestamp = 0
 
     def start(self, dt, t_end):
         """ Starts the simulation """
-        
+
         for t in np.arange(0, t_end, dt):
             self.timestap = t
             self.grid.announce_beforestep()
-            self.grid.announce_step
-            
+            self.grid.announce_step()
+
             self.iteration_callback()
-            history.append(self.get_snapshot())
-        
+            self.history.append(self.get_snapshot())
+
         return
-    
+
     def get_snapshot(self):
         """" Returns a array of dictionaries with all the cell states"""
         data = np.array([])
         for cell in self.grid.children:
-            celldata = {"t", self.timestamp "id" : cell.id, "age": cell.age, "parent_ring": cell.parent.id, "theta1" : cell.theta1, "theta2" : cell.theta2}
+            celldata = {
+                "t": self.timestamp,
+                "id": cell.id,
+                "age": cell.current_age,
+                "parent_ring": cell.parent.id,
+                "theta1": cell.theta1,
+                "theta2": cell.theta2,
+            }
             data.append(celldata)
         return data
-    def pause():
+
+    def pause(self):
         """"Pauses the current simulation after finishing current iteration"""
         self.started = False
 
