4b75a93e6d8ba4581690e90bc3b7a9fb3a9a3d05
Kasper van Tulder <kvtulder@gmail.com>
2021-06-27 14:38:30 +0200

    better documentation

--- analyse.py
+++ analyse.py
@@ -2,22 +2,19 @@ import numpy as np
 
 
 def starFormationRate(df, regenTime):
-    """" Returns the amount of new formed stars per timestep"""
-
+    """" Returns the number of new formed stars for each timeframe"""
 
     frames = df["t"].max()
-    formationRate = np.zeros(frames)
+    formationrate = np.zeros(frames)
     for i in range(frames):
         for index, row in df[(df.t == i) & (df.age == regenTime)].iterrows():
-            formationRate[int(row["t"])] += 1
+            formationrate[int(row["t"])] += 1
 
-    return formationRate
+    return formationrate
 
 
 def convergenceCheck(starformation):
-    """
-    Searches for the stable region of s
-    """
+    """Searches for the stable region of s. Returns the list starting from the stabilized point"""
     windowsize = int(len(starformation) / 10)
     if windowsize < 2:
         windowsize = 2
@@ -29,15 +26,15 @@ def convergenceCheck(starformation):
 
     while not converged:
 
-        temp_starformation_first = starformation_reversed[i : windowsize + i]
+        temp_starformation_first = starformation_reversed[i:windowsize + i]
         mean_first = np.array(temp_starformation_first).mean()
         i += 1
-        temp_starformation_second = starformation_reversed[i : windowsize + i]
+        temp_starformation_second = starformation_reversed[i:windowsize + i]
 
         mean_second = np.array(temp_starformation_second).mean()
 
         if abs(mean_first - mean_second) > amplitude:
-            index = len(starformation) - (i)
+            index = len(starformation) - i
             converged = True
 
         if windowsize + i == len(starformation):
